b5522f1cfbefaa76b8f3d080dcb47adb73e689da
g943_shishkin.a.i <astopikov@gmail.com>
2022-12-19 00:29:39 +0300

    feat: del main and controller classes

--- src/main/java/com/shishkin/Main.java
+++
@@ -1,9 +0,0 @@
-package com.shishkin;
-
-import com.shishkin.controller.TradingController;
-
-public class Main {
-    public static void main(String[] args) {
-        TradingController.start();
-    }
-}
